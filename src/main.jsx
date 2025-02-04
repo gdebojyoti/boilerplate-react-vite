7f8b92d6015ed755bbd9e81459d07d83aca2da39
Debojyoti Ghosh <gdebojyoti@users.noreply.github.com>
2025-02-04 13:51:44 +0530

    Route added for Home page.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router'
+
 import './index.css'
-import App from './App.jsx'
+import Home from './pages/Home/index.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' Component={Home} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import './style.css'
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1 className="home__heading">Welcome Home!</h1>
+    </div>
+  )
+}
+
+export default Home
